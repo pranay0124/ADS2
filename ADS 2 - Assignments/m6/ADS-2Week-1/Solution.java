066a18936bd90693a9a73403ec6981ba541d72fb
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-11-03 12:10:28 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 2 - Assignments/m6/ADS-2Week-1/Solution.java
+++ ADS 2 - Assignments/m6/ADS-2Week-1/Solution.java
@@ -1,28 +1,34 @@
 
 class PageRank {
 	private Digraph pagerankGraph;
-	private double[] pagerankValue;
+	private double[] values;
+	private double[] finalvalues;
 	// private int ver = pagerankGraph.V();
 	PageRank(Digraph graph) {
 		this.pagerankGraph = graph;
-		pagerankValue = new double[pagerankGraph.V()];
-		for(int i=0; i< pagerankValue.length;i++) {
-			System.out.println(pagerankValue[i] + "before");
-			pagerankValue[i] = (1.0/(pagerankGraph.V()));
-			System.out.println(pagerankValue[i] + "after");
+		values = new double[pagerankGraph.V()];
+		finalvalues = new double[pagerankGraph.V()];
+		for (int i = 0; i < values.length; i++) {
+			// System.out.println(pagerankValue[i] + "before");
+			values[i] = (1.0 / (double)(pagerankGraph.V()));
+			// System.out.println(pagerankValue[i] + "after");
 		}
 		updateValue();
 	}
 
 	void updateValue() {
-		for(int i=0;i<1000;i++) {
-			for(int j=0;i<pagerankGraph.V();j++) {
+		for (int i = 0; i < 1000; i++) {
+			for (int j = 0; i < pagerankGraph.V(); j++) {
 				double test = 0.0;
-				for(Integer each : pagerankGraph.adj(j)) {
-					test += (pagerankValue[each]/pagerankGraph.outdegree(each));
+				for (Integer each : pagerankGraph.adj(j)) {
+					test += (values[each] / pagerankGraph.outdegree(each));
 				}
-				pagerankValue[j] = test;
+				finalvalues[j] = test;
 			}
+			values = finalvalues.clone();
+		}
+		for (int k = 0; k < finalvalues.length; k++) {
+			System.out.print(k + " : " + finalvalues[k] + "\n");
 		}
 	}
 }
@@ -36,37 +42,37 @@ public class Solution {
 	public static void main(String[] args) {
 		// read the first line of the input to get the number of vertices
 		int vertices = Integer.parseInt(StdIn.readLine());
-		// iterate count of vertices times 
+		// iterate count of vertices times
 		// to read the adjacency list from std input
 		// and build the graph
 		// int verticescopy = vertices;
 		Digraph digraph = new Digraph(vertices);
-		for(int i=0; i<vertices; i++) {
+		for (int i = 0; i < vertices; i++) {
 			String[] vertex = StdIn.readLine().split(" ");
-			for(int j =1;j<vertex.length;j++) {
+			for (int j = 1; j < vertex.length; j++) {
 				digraph.addEdge(Integer.parseInt(vertex[0]), Integer.parseInt(vertex[j]));
 			}
 		}
 		System.out.println(digraph);
-		
+
 		// Create page rank object and pass the graph object to the constructor*/
 		PageRank pagerank = new PageRank(digraph);
-		
+
 		// print the page rank object
 		System.out.println(pagerank);
-		
+
 		// This part is only for the final test case
-		
+
 		// File path to the web content
 		String file = "WebContent.txt";
-		
+
 		// instantiate web search object
 		// and pass the page rank object and the file path to the constructor
-		
+
 		// read the search queries from std in
 		// remove the q= prefix and extract the search word
 		// pass the word to iAmFeelingLucky method of web search
 		// print the return value of iAmFeelingLucky
-		
+
 	}
 }
