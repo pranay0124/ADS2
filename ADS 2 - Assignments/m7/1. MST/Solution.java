c7dd5927b42a7937bb3656fbb8e476bf807a27a5
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-11-05 23:34:55 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS 2 - Assignments/m7/1. MST/Solution.java
+++ ADS 2 - Assignments/m7/1. MST/Solution.java
@@ -7,26 +7,28 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() { }
-	/**
-	 * Main FUnction.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		EdgeWeightedGraph edgewtGraph = new EdgeWeightedGraph(vertices);
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(" ");
-			edgewtGraph.addEdge(new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2])));
-		}
-		KruskalMST kruskal = new KruskalMST(edgewtGraph);
-		System.out.format("%.5f", kruskal.weight());
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * Main FUnction.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        EdgeWeightedGraph edgewtGraph = new EdgeWeightedGraph(vertices);
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            edgewtGraph.addEdge(new Edge(Integer.parseInt(tokens[0]),
+                                         Integer.parseInt(tokens[1]),
+                                         Double.parseDouble(tokens[2])));
+        }
+        KruskalMST kruskal = new KruskalMST(edgewtGraph);
+        System.out.format("%.5f", kruskal.weight());
+    }
 }
 
