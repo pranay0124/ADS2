bb2b55cc07b94c6abb4920018c5ef16e39055512
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-11-10 12:23:05 +0530

    "ads2_m12_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS 2 - Assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS 2 - Assignments/m12/ADS-2_Exam-2/Solution.java
@@ -17,9 +17,6 @@ public final class Solution {
 	 * @param      args  The arguments
 	 */
 	public static void main(final String[] args) {
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
 		Scanner scan = new Scanner(System.in);
 		int vertices = Integer.parseInt(scan.nextLine());
 		int edges = Integer.parseInt(scan.nextLine());
@@ -39,11 +36,6 @@ public final class Solution {
 			break;
 
 		case "DirectedPaths":
-			// Handle the case of DirectedPaths,
-			//                     where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
 			String[] path = scan.nextLine().split(" ");
 			int s = Integer.parseInt(path[0]);
 			int d = Integer.parseInt(path[1]);
@@ -56,12 +48,6 @@ public final class Solution {
 			break;
 
 		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where
-			//                                    path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
 			String[] paths = scan.nextLine().split(" ");
 			int source = Integer.parseInt(paths[0]);
 			int via = Integer.parseInt(paths[1]);
