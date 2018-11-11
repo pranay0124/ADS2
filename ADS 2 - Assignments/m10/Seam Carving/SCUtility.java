3cc811efa7711e5ae18ed68c9be72907b4465bb0
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-11-11 23:24:55 +0530

    "ads2_m10_a1_1: 6/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 2 - Assignments/m10/Seam Carving/SCUtility.java
+++ ADS 2 - Assignments/m10/Seam Carving/SCUtility.java
@@ -36,8 +36,8 @@ public class SCUtility {
         for (int col = 0; col < sc.width(); col++)
             for (int row = 0; row < sc.height(); row++)
                 returnDouble[col][row] = sc.energy(col, row);
-    
-        return returnDouble;        
+
+        return returnDouble;
     }
 
     // displays grayvalues as energy (converts to picture, calls show)
@@ -69,7 +69,7 @@ public class SCUtility {
                     maxVal = grayValues[col][row];
             }
         }
-            
+
         if (maxVal == 0)
             return picture; // return black picture
 
@@ -96,7 +96,7 @@ public class SCUtility {
         for (int col = 0; col < width; col++)
             for (int row = 0; row < height; row++)
                 overlaid.set(col, row, picture.get(col, row));
-        
+
 
         // if horizontal seam, then set one pixel in every column
         if (horizontal) {
