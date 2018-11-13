1f7156da9063e60335c787185ec5a23980c4bd5b
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-11-13 19:37:12 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 2 - Assignments/m14/Assignment-1/Solution.java
+++ ADS 2 - Assignments/m14/Assignment-1/Solution.java
@@ -1,27 +1,48 @@
+/**
+ * @author : Pranay Kumar Y.
+ * Date : 13th November,2018.
+ */
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] words = loadWords();
+        TST<Integer> tst = new TST<Integer>();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                String str = words[i].substring(j);
+                if (!tst.contains(str)) {
+                    tst.put(str, i);
+                }
+            }
+        }
+        String input = scan.nextLine();
+        for (String s : tst.keysWithPrefix(input)) {
+            System.out.println(s);
+        }
+    }
 
-public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String[] words = loadWords();
-		TST<Integer> tst = new TST<Integer>();
-		for (int i=0;i<words.length;i++) {
-			for(int j=0;j<words[i].length();j++) {
-				String str = words[i].substring(j);
-				if(!tst.contains(str)) {
-					tst.put(str, i);
-				}
-			}
-		}
-		String input = scan.nextLine();
-		for(String s : tst.keysWithPrefix(input)) {
-			System.out.println(s);
-		}
-	}
-
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+    /**
+     * Loads words.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
+
