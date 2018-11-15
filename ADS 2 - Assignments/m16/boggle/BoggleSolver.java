d616954576b8496b41a06fe45bedd502719594c6
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-11-15 17:46:20 +0530

    "ads2_m16_a1_1: 2/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS 2 - Assignments/m16/boggle/BoggleSolver.java
+++ ADS 2 - Assignments/m16/boggle/BoggleSolver.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class BoggleSolver {
 	// Initializes the data structure using the given array of strings as the dictionary.
 	TST<Integer> tst;
@@ -11,11 +12,39 @@ public class BoggleSolver {
 
 	// Returns the set of all valid words in the given Boggle board, as an Iterable.
 	public Iterable<String> getAllValidWords(BoggleBoard board) {
-		return new Bag<String>();
+		ArrayList<String> bag = new ArrayList<String>();
+		boolean[][] marked = new boolean[board.rows()][board.cols()];
+		for (int i = 0; i < board.rows(); i++) {
+			for (int j = 0; j < board.cols(); j++) {
+				marked[i][j] = true;
+				String str = board.getLetter(i, j) + "";
+				dfs(i, j, str, marked, board, bag);
+			}
+		}
+		return bag;
 	}
 
-	// Returns the score of the given word if it is in the dictionary, zero otherwise.
-	// (You can assume the word contains only the uppercase letters A through Z.)
+	public void dfs(int row, int col, String str, boolean[][] marked, BoggleBoard board, ArrayList<String> bag) {
+		if (str.length() > 2 && tst.contains(str)) {
+			if (!bag.contains(str)) {
+				bag.add(str);
+			}
+		}
+		for (int i = 0; i < board.rows(); i++) {
+			for (int j = 0; j < board.cols(); j++) {
+				if (marked[i][j]) {
+					continue;
+				}
+				marked[i][j] = true;
+				str += board.getLetter(i, j) + "";
+				dfs(i, j, str, marked, board, bag);
+			}
+		}
+	}
+
+
+// Returns the score of the given word if it is in the dictionary, zero otherwise.
+// (You can assume the word contains only the uppercase letters A through Z.)
 	public int scoreOf(String word) {
 		int a = word.length();
 		if (a >= 0 || a <= 2) {
