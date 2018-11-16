0852bfe1889b71af38b5100d98fc865f7529438e
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-11-16 17:08:09 +0530

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 2 - Assignments/m16/boggle/BoggleSolver.java
+++ ADS 2 - Assignments/m16/boggle/BoggleSolver.java
@@ -1,18 +1,39 @@
 import java.util.ArrayList;
+/**
+ * Class for boggle solver.
+ */
 public class BoggleSolver {
-	// Initializes the data structure using the given array of strings as the dictionary.
-	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
-	TST<Integer> tstDict;
+	// Initializes the data structure using the
+	// given array of strings as the dictionary.
+	//
+	// (You can assume each word in the dictionary
+	// contains only the uppercase letters A through Z.)
 
-	public BoggleSolver(String[] dictionary) {
+	/**
+	 * TST to store values.
+	 */
+	TST<Integer> tstDict;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      dictionary  The dictionary
+	 */
+	public BoggleSolver(final String[] dictionary) {
 		tstDict = new TST<Integer>();
 		for (int i = 0; i < dictionary.length; i++) {
 			tstDict.put(dictionary[i], i);
 		}
 	}
 
-	// Returns the set of all valid words in the given Boggle board, as an Iterable.
-	public Iterable<String> getAllValidWords(BoggleBoard board) {
+	/**
+	 * Gets all valid words.
+	 * Returns the set of all valid words in the given Boggle board.
+	 *
+	 * @param      board  The board
+	 *
+	 * @return     All valid words.
+	 */
+	public Iterable<String> getAllValidWords(final BoggleBoard board) {
 		ArrayList<String> arraylist = new ArrayList<String>();
 		for (int i = 0; i < board.rows(); i++) {
 			for (int j = 0; j < board.cols(); j++) {
@@ -23,17 +44,32 @@ public class BoggleSolver {
 		}
 		return arraylist;
 	}
+	/**
+	 * Depth first search.
+	 *
+	 * @param      board      The board
+	 * @param      arraylist  The arraylist
+	 * @param      marked     The marked
+	 * @param      row        The row
+	 * @param      col        The col
+	 * @param      str        The string
+	 */
+	public void dfs(BoggleBoard board, ArrayList<String> arraylist,
+	                boolean[][] marked,
+	                int row, int col, String str) {
 
-	public void dfs(BoggleBoard board, ArrayList<String> arraylist, boolean[][] marked, int row, int col, String str) {
 		marked[row][col] = true;
+
 		if (board.getLetter(row, col) == 'Q') {
 			str += board.getLetter(row, col) + "U";
 		} else {
 			str += board.getLetter(row, col);
 		}
+
 		if (!tstDict.hasPrefix(str)) {
 			return;
 		}
+		
 		if (str.length() > 2 && tstDict.contains(str)) {
 			if (!arraylist.contains(str)) {
 				arraylist.add(str);
@@ -41,7 +77,7 @@ public class BoggleSolver {
 		}
 		for (int i = row - 1; i < row + 2; i++) { // i < row-1 + 3
 			for (int j = col - 1; j < col + 2; j++) {
-				if (validate(i, j, board) && !(marked[i][j])) {
+				if ((i >= 0 && i < board.rows() && j >= 0 && j < board.cols()) && !(marked[i][j])) {
 					dfs(board, arraylist, marked, i, j, str);
 					marked[i][j] = false;
 				}
@@ -49,12 +85,12 @@ public class BoggleSolver {
 		}
 	}
 
-	public boolean validate(int i, int j, BoggleBoard board) {
-		if (i >= 0 && i < board.rows() && j >= 0 && j < board.cols()) {
-			return true;
-		}
-		return false;
-	}
+	// public boolean validate(int i, int j, BoggleBoard board) {
+	// 	if (i >= 0 && i < board.rows() && j >= 0 && j < board.cols()) {
+	// 		return true;
+	// 	}
+	// 	return false;
+	// }
 
 
 // Returns the score of the given word if it is in the dictionary, zero otherwise.
